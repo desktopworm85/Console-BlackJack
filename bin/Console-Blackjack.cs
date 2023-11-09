b617aec86c68de3ce8848e270f38e13f93114249
desktopworm <39500730+desktopworm85@users.noreply.github.com>
2023-11-09 12:13:55 +0000

    Update Console-Blackjack.cs

--- bin/Console-Blackjack.cs
+++ bin/Console-Blackjack.cs
@@ -14,11 +14,14 @@ namespace ConsoleBlackJack {
         public static int numDealerTurns = 0;
         public static int dealerVal = 0;
         public static int dealerAces = 0;
+        public static bool dealerStanding = false;
 
         public static int cash;
-        public static bool turn = false; //true for player  false for dealer
+        public static bool turn = true; //true for player  false for dealer
 
         public static int[] drawnCards = new int[13]; // 0 Ace 11 Jack 12 Queen 13 King
+
+        public static int[] probabilities = {30, 65, 85, 90, 100};
 
         static string RandomCardDraw(bool whoDraw /*true, player draw. false, dealer draw*/) {
             Random rand = new Random();
@@ -85,13 +88,23 @@ namespace ConsoleBlackJack {
                 }
             }
         }
-
+        
         static void game() {
             if (turn) {
                 Console.Clear();
                 Console.Write("Your current cards: ");
                 writeArray(playersHand, numPlayerTurns);
-                Console.Write("   The value of you hand is: " + Convert.ToString(playerVal))
+                Console.Write("   The value of your hand is: " + Convert.ToString(playerVal) + "\n");
+                Console.WriteLine();
+                Console.Write("The dealers hand: " + dealersHand[0]);
+                for (int i = 1; i < numDealerTurns; i++) {
+                    Console.Write("*");
+                    if (i == numDealerTurns-1) {
+                        Console.Write("\n");
+                    } else {
+                        Console.Write(", ");
+                    }
+                }
                 Console.WriteLine();
                 Console.WriteLine("What would you like to do? Hit/Stand");
                 string? response = Console.ReadLine();
@@ -99,10 +112,95 @@ namespace ConsoleBlackJack {
                     if (response.ToUpper() == "HIT") {
                         playersHand[numPlayerTurns] = RandomCardDraw(true);
                         numPlayerTurns++;
+                        
+                        if (playerVal > 21) {
+                            if (playerAces > 0) {
+                                playerVal -= 10 * playerAces;
+                                playerAces = 0;
+                                if (playerVal > 21) {
+                                    Console.WriteLine();
+                                    Console.WriteLine("You drew an ace but you were already on 21, thus you lose.");
+                                }
+                            } else {
+                                Console.WriteLine();
+                                Console.WriteLine("You drew a " + playersHand[numPlayerTurns - 1] + " which set you over the value of 21. You lose");
+                            }
+                        } else {
+                            turn = false;
+                            game();
+                        }
+                    } else if (response.ToUpper() == "STAND") {
+                        Console.WriteLine();
+                        Console.WriteLine("You have decided to stand.");
+                        Thread.Sleep(2000);
+                        turn = false;
+                        game();
                     }
                 } else {
                     Console.WriteLine("You cannot input a null value, try again.");
                     Thread.Sleep(3000);
+                    game();
+                }
+            } else {
+                if (!dealerStanding) {
+                    if (dealerVal < 17) {
+                        dealersHand[numDealerTurns] = RandomCardDraw(false);
+                        numDealerTurns++;
+                    }
+
+                    if (dealerVal >= 17) {
+                        Random rand = new Random();
+                        int randProb = rand.Next(101);
+                        bool willHit = false;
+                        switch(dealerVal) {
+                            case 17:
+                                if (randProb >= probabilities[0]) {
+                                    willHit = true;
+                                }
+                                break;
+                            case 18:
+                                if (randProb >= probabilities[1]) {
+                                    willHit = true;
+                                }
+                                break;
+                            case 19:
+                                if (randProb >= probabilities[2]) {
+                                    willHit = true;
+                                }
+                                break;
+                            case 20:
+                                if (randProb >= probabilities[3]) {
+                                    willHit = true;
+                                }
+                                break;
+                            case 21:
+                                if (randProb >= probabilities[4]) {
+                                    willHit = true;
+                                }
+                                break;
+                            default:
+                                Console.WriteLine("Something went wrong with the ai.");
+                                Thread.Sleep(10000);
+                                break;
+                        }
+
+                        if (willHit) {
+                            dealersHand[numDealerTurns] = RandomCardDraw(false);
+                            numDealerTurns++;
+                        }
+                    }
+
+                    if (dealerVal > 21) {
+                        Console.WriteLine("The dealer has gone over the value of 21, you win!");
+                    } else {
+                        turn = true;
+                        game();
+                    }
+                } else {
+                    Console.WriteLine();
+                    Console.WriteLine("The dealer has decided to stand.");
+                    Thread.Sleep(3000);
+                    turn = true;
                     game();
                 }
             }
@@ -123,6 +221,9 @@ namespace ConsoleBlackJack {
             dealersHand[1] = RandomCardDraw(false);
             numPlayerTurns = 2;
             numDealerTurns = 2;
+            game();
         }
     }
 }
+
+/* TODO: If both players stand, end game. Try catch for line 94 to let the code run if it is in vsCode debug*/
